4427ddec414fb4065e2229883736a774b220b3e2
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-07-09 14:24:06 +1000

    Update

--- src/utils/stripe.ts
+++ src/utils/stripe.ts
@@ -1,9 +1,14 @@
 import Stripe from "stripe";
 
 // Initialize Stripe
-export const stripe = new Stripe(process.env.STRIPE_SECRET_TEST as string, {
-    apiVersion: "2020-08-27",
-});
+export const stripe = new Stripe(
+    process.env.NODE_ENV !== "production"
+        ? (process.env.STRIPE_SECRET_TEST as string)
+        : (process.env.STRIPE_SECRET as string),
+    {
+        apiVersion: "2020-08-27",
+    }
+);
 
 export interface CatalogueItem {
     price: Stripe.Price;
