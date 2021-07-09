db37da9c93358809c7b6e6012328d0d96f72d3fd
BenGOsborn <bengrantalbertosborn@gmail.com>
2021-07-09 21:22:05 +1000

    Update

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,7 +2,7 @@
 export const siteURL: string =
     process.env.NODE_ENV !== "production"
         ? "http://localhost:3000"
-        : "https://siteurl.com";
+        : "https://lotion-shop.herokuapp.com";
 
 // The max quantity of items a user can add to their cart
 export const MAX_QUANTITY = 15;
